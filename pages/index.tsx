15842843fcee6fa9c07d4db17993eab58e83873e
Brent Mitton <brent.mitton+github@gmail.com>
2020-09-15 17:57:29 -0400

    init a bunch of assets

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,14 @@
-const IndexPage = () => <p> Hi </p>;
+import Head from "next/head";
+const IndexPage = () => {
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="description" content="Kate & Chris are getting married." />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+    </>
+  );
+};
 
 export default IndexPage;
