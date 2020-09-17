79346a1db6f373efd2fa803eb5c01c740469e857
Brent Mitton <brent.mitton+github@gmail.com>
2020-09-17 14:35:26 -0400

    put in the tina providers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,78 @@ import "../css/bootstrap.css";
 import "../css/magnific-popup.css";
 import "../css/fonts.css";
 
-import { AppProps } from "next/app";
+import { GithubClient, TinacmsGithubProvider } from "react-tinacms-github";
+import { TinaCMS, TinaProvider } from "tinacms";
 
-const App = ({ Component, pageProps }: AppProps): any => (
-  <Component {...pageProps} />
-);
-export default App;
+import App from "next/app";
+
+export default class Site extends App {
+  cms: TinaCMS;
+
+  constructor(props: any) {
+    super(props);
+
+    this.cms = new TinaCMS({
+      enabled: !!props.pageProps.preview,
+      apis: {
+        github: new GithubClient({
+          proxy: "/api/proxy-github",
+          authCallbackRoute: "/api/create-github-access-token",
+          clientId: process.env.GITHUB_CLIENT_ID || "",
+          baseRepoFullName: process.env.REPO_FULL_NAME || "",
+        }),
+      },
+      sidebar: props.pageProps.preview,
+      toolbar: props.pageProps.preview,
+    });
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <TinaProvider cms={this.cms}>
+        <TinacmsGithubProvider
+          onLogin={onLogin}
+          onLogout={onLogout}
+          error={pageProps.error}
+        >
+          <EditLink cms={this.cms} />
+          <Component {...pageProps} />
+        </TinacmsGithubProvider>
+      </TinaProvider>
+    );
+  }
+}
+
+const onLogin = async () => {
+  const token = localStorage.getItem("tinacms-github-token") || null;
+  const headers = new Headers();
+
+  if (token) {
+    headers.append("Authorization", "Bearer " + token);
+  }
+
+  const resp = await fetch(`/api/preview`, { headers: headers });
+  const data = await resp.json();
+
+  if (resp.status == 200) window.location.href = window.location.pathname;
+  else throw new Error(data.message);
+};
+
+const onLogout = () => {
+  return fetch(`/api/reset-preview`).then(() => {
+    window.location.reload();
+  });
+};
+
+export interface EditLinkProps {
+  cms: TinaCMS;
+}
+
+export const EditLink = ({ cms }: EditLinkProps) => {
+  return (
+    <button onClick={() => cms.toggle()}>
+      {cms.enabled ? "Exit Edit Mode" : "Edit This Site"}
+    </button>
+  );
+};
