da6143e41a62e63fd3dba48bb313f2d690fbf4c6
Brent Mitton <brent.mitton+github@gmail.com>
2020-10-20 16:24:22 -0400

    add preview images

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -6,12 +6,12 @@ const Gallery = () => {
           <div className="row justify-content-center card-gutters">
             <div className="col-7 mb-3">
               <a
-                href="assets/img/gallery/1.JPG"
+                href="assets/img/gallery/2.JPG"
                 className="popup-image hover-effect d-block"
               >
                 <img
                   className="img-fluid"
-                  src="assets/img/gallery/1.JPG"
+                  src="assets/img/gallery/2_preview.jpg"
                   alt=""
                 />
                 <span className="hover-effect-container">
@@ -49,12 +49,12 @@ const Gallery = () => {
             </div>
             <div className="col-5 align-self-end mb-3">
               <a
-                href="assets/img/gallery/2.JPG"
+                href="assets/img/gallery/1.JPG"
                 className="popup-image hover-effect d-block"
               >
                 <img
                   className="img-fluid"
-                  src="assets/img/gallery/2.JPG"
+                  src="assets/img/gallery/1_preview.jpg"
                   alt=""
                 />
                 <span className="hover-effect-container">
@@ -97,50 +97,7 @@ const Gallery = () => {
               >
                 <img
                   className="img-fluid"
-                  src="assets/img/gallery/3.JPG"
-                  alt=""
-                />
-                <span className="hover-effect-container">
-                  <span className="hover-effect-icon has-svg">
-                    <svg
-                      version="1.1"
-                      xmlBase="http://www.w3.org/2000/svg"
-                      xmlnsXlink="http://www.w3.org/1999/xlink"
-                      x="0px"
-                      y="0px"
-                      viewBox="0 0 73.6404 64.5606"
-                      enable-background="new 0 0 73.6404 64.5606"
-                      xmlSpace="preserve"
-                    >
-                      <g>
-                        <path
-                          fill="#636784"
-                          d="M36.904,6.66L36.904,6.66C28.5329-1.7395,14.9401-1.8373,6.4502,6.4736
-                                        c-8.5632,8.3827-8.5871,22.1694-0.2037,30.724l24.4097,24.9081c3.1797,3.2446,8.3876,3.2973,11.6323,0.1176l24.7745-24.2787
-                                        c8.5602-8.3889,8.832-22.3842,0.4549-30.956C59.1567-1.5672,45.4453-1.7103,36.904,6.66z"
-                        />
-                        <path
-                          fill="#FFFFFF"
-                          d="M44.695,28.8005c0-0.4883-0.3996-0.8879-0.8879-0.8879h-4.4394v-4.4394
-                                        c0-0.4883-0.3996-0.8879-0.8879-0.8879h-2.9596c-0.4883,0-0.8879,0.3996-0.8879,0.8879v4.4394h-4.4394
-                                        c-0.4883,0-0.8879,0.3995-0.8879,0.8879v2.9596c0,0.4883,0.3996,0.8879,0.8879,0.8879h4.4394v4.4394
-                                        c0,0.4883,0.3996,0.8879,0.8879,0.8879h2.9596c0.4883,0,0.8879-0.3996,0.8879-0.8879V32.648h4.4394
-                                        c0.4883,0,0.8879-0.3995,0.8879-0.8879V28.8005z"
-                        />
-                      </g>
-                    </svg>
-                  </span>
-                </span>
-              </a>
-            </div>
-            <div className="col-3">
-              <a
-                href="assets/img/gallery/4.JPG"
-                className="popup-image hover-effect d-block"
-              >
-                <img
-                  className="img-fluid"
-                  src="assets/img/gallery/4.JPG"
+                  src="assets/img/gallery/3_preview.jpg"
                   alt=""
                 />
                 <span className="hover-effect-container">
@@ -183,7 +140,50 @@ const Gallery = () => {
               >
                 <img
                   className="img-fluid"
-                  src="assets/img/gallery/5.JPG"
+                  src="assets/img/gallery/5_preview.jpg"
+                  alt=""
+                />
+                <span className="hover-effect-container">
+                  <span className="hover-effect-icon has-svg">
+                    <svg
+                      version="1.1"
+                      xmlBase="http://www.w3.org/2000/svg"
+                      xmlnsXlink="http://www.w3.org/1999/xlink"
+                      x="0px"
+                      y="0px"
+                      viewBox="0 0 73.6404 64.5606"
+                      enable-background="new 0 0 73.6404 64.5606"
+                      xmlSpace="preserve"
+                    >
+                      <g>
+                        <path
+                          fill="#636784"
+                          d="M36.904,6.66L36.904,6.66C28.5329-1.7395,14.9401-1.8373,6.4502,6.4736
+                                        c-8.5632,8.3827-8.5871,22.1694-0.2037,30.724l24.4097,24.9081c3.1797,3.2446,8.3876,3.2973,11.6323,0.1176l24.7745-24.2787
+                                        c8.5602-8.3889,8.832-22.3842,0.4549-30.956C59.1567-1.5672,45.4453-1.7103,36.904,6.66z"
+                        />
+                        <path
+                          fill="#FFFFFF"
+                          d="M44.695,28.8005c0-0.4883-0.3996-0.8879-0.8879-0.8879h-4.4394v-4.4394
+                                        c0-0.4883-0.3996-0.8879-0.8879-0.8879h-2.9596c-0.4883,0-0.8879,0.3996-0.8879,0.8879v4.4394h-4.4394
+                                        c-0.4883,0-0.8879,0.3995-0.8879,0.8879v2.9596c0,0.4883,0.3996,0.8879,0.8879,0.8879h4.4394v4.4394
+                                        c0,0.4883,0.3996,0.8879,0.8879,0.8879h2.9596c0.4883,0,0.8879-0.3996,0.8879-0.8879V32.648h4.4394
+                                        c0.4883,0,0.8879-0.3995,0.8879-0.8879V28.8005z"
+                        />
+                      </g>
+                    </svg>
+                  </span>
+                </span>
+              </a>
+            </div>
+            <div className="col-3">
+              <a
+                href="assets/img/gallery/4.JPG"
+                className="popup-image hover-effect d-block"
+              >
+                <img
+                  className="img-fluid"
+                  src="assets/img/gallery/4_preview.jpg"
                   alt=""
                 />
                 <span className="hover-effect-container">
