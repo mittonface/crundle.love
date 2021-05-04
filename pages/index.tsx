e043a3c83e67fe7bc5f0fabc93fcecacd975a5ec
Brent Mitton <brent.mitton+github@gmail.com>
2021-05-04 09:33:36 -0400

    link first piece of content

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,41 @@ import EventDetails from "../components/EventDetails";
 import Gallery from "../components/Gallery";
 import Head from "next/head";
 import Hero from "../components/Hero";
+import { Hero_Document } from "../.tina/__generated__/types";
+import { LocalClient } from "tina-graphql-gateway";
 import RSVP from "../components/RSVP";
 import Registry from "../components/Registry";
 import Timeline from "../components/Timeline";
 import WeddingParty from "../components/WeddingParty";
-const IndexPage = () => {
+
+export const getStaticProps = async () => {
+  const client = new LocalClient();
+  return {
+    props: await client.request(query, {
+      variables: {},
+    }),
+  };
+};
+
+export const query = (gql: any) => gql`
+  query PageQuery {
+    getHeroDocument(relativePath: "main.md") {
+      data {
+        __typename
+        ... on Main_Doc_Data {
+          main
+          _body
+        }
+      }
+    }
+  }
+`;
+
+export type PageQueryResponseType = {
+  getHeroDocument: Hero_Document;
+};
+
+const IndexPage = (props: PageQueryResponseType) => {
   return (
     <>
       <Head>
@@ -101,7 +131,7 @@ const IndexPage = () => {
           </div>
         </header>
 
-        <Hero />
+        <Hero heroDoc={props.getHeroDocument} />
         <DateSection />
         <Timeline />
         <EventDetails />
