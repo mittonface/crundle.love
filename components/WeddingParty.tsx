c308f2fcf67801786185d78d1814bb88991d59e7
Brent Mitton <brent.mitton+github@gmail.com>
2021-06-22 21:22:06 -0400

    update wedding party

--- components/WeddingParty.tsx
+++ components/WeddingParty.tsx
@@ -6,90 +6,31 @@ const WeddingParty = () => {
           <div className="col">
             <div className=" mb-5 pb-5">
               <h1 className="display-4 mb-0">Our Wedding Party</h1>
-              <p className="w-md-40 mb-0 mx-auto text-dark-gray opacity-8 ">
-                They're very good looking.
-              </p>
             </div>
           </div>
         </div>
         <div className="row justify-content-center">
           <div className="col-md-4 d-flex flex-column align-items-center">
             <div className="px-5 mb-4 pb-4 ">
-              <div className="svg-mask-container px-3">
-                <svg
-                  width="100%"
-                  height="100%"
-                  version="1.1"
-                  xmlBase="http://www.w3.org/2000/svg"
-                  xmlnsXlink="http://www.w3.org/1999/xlink"
-                  x="0px"
-                  y="0px"
-                  viewBox="0 0 607 532"
-                  enable-background="new 0 0 607 532"
-                  xmlSpace="preserve"
-                >
-                  <defs>
-                    <mask id="mask-middle-1">
-                      <path
-                        fill="#FFFFFF"
-                        d="M303.2954,55.7655L303.2954,55.7655c-68.9887-68.3032-180.269-68.3601-249.3155,0.1402
-                                  c-69.6423,69.0921-69.0838,181.9562,0.0139,251.5284l201.1888,202.5704c26.2076,26.3876,68.8445,26.5335,95.2321,0.3258
-                                  l201.4838-200.1096c69.6179-69.1431,71.0768-183.7278,2.0303-253.4415C485.013-12.8027,372.7591-13.2244,303.2954,55.7655z"
-                      ></path>
-                    </mask>
-                  </defs>
-                  <image
-                    mask="url(#mask-middle-1)"
-                    width="607"
-                    height="532"
-                    xlinkHref="assets/img/mask-md.jpg"
-                  ></image>
-                </svg>
-              </div>
               <div className=" mt-4">
-                <h5 className="mb-0">
-                  Amanda <s>Rundle</s> Bettle
-                </h5>
-                <span className="small">Matron of Honour</span>
+                <ul style={{ listStyle: "none" }}>
+                  <li>Emma Barlow</li>
+                  <li>Amanda Bettle (Matron of Honour)</li>
+                  <li>Cierra Gaudet</li>
+                  <li>Sara Reeves</li>
+                </ul>
               </div>
             </div>
           </div>
           <div className="col-md-4 d-flex flex-column align-items-center">
             <div className=" px-5 mb-4 pb-4 ">
-              <div className="svg-mask-container px-3">
-                <svg
-                  width="100%"
-                  height="100%"
-                  version="1.1"
-                  xmlBase="http://www.w3.org/2000/svg"
-                  xmlnsXlink="http://www.w3.org/1999/xlink"
-                  x="0px"
-                  y="0px"
-                  viewBox="0 0 607 532"
-                  enable-background="new 0 0 607 532"
-                  xmlSpace="preserve"
-                >
-                  <defs>
-                    <mask id="mask-middle-2">
-                      <path
-                        fill="#FFFFFF"
-                        d="M303.2954,55.7655L303.2954,55.7655c-68.9887-68.3032-180.269-68.3601-249.3155,0.1402
-                                  c-69.6423,69.0921-69.0838,181.9562,0.0139,251.5284l201.1888,202.5704c26.2076,26.3876,68.8445,26.5335,95.2321,0.3258
-                                  l201.4838-200.1096c69.6179-69.1431,71.0768-183.7278,2.0303-253.4415C485.013-12.8027,372.7591-13.2244,303.2954,55.7655z"
-                      ></path>
-                    </mask>
-                  </defs>
-                  <image
-                    mask="url(#mask-middle-2)"
-                    width="607"
-                    height="532"
-                    xlinkHref="assets/img/mask-md.jpg"
-                  ></image>
-                </svg>
-              </div>
               <div className=" mt-4">
-                <h5 className="mb-0">Brent Mitton</h5>
-                <span className="small">Best Man</span>
+                <ul style={{ listStyle: "none" }}>
+                  <li>Devin MacDougall</li>
+                  <li>Brent Mitton (Best Man)</li>
+                  <li>Nolan Phillips </li>
+                  <li>David Rundle </li>
+                </ul>
               </div>
             </div>
           </div>
